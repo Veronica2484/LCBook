b2d2b82e9864ac81bc56518d4b0659774ed15bb0
Veronica2484 <veronicacondori24@gmail.com>
2021-07-26 20:17:04 +0100

    Add logout function and push the user to login

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,28 +1,62 @@
 //we import link from router dom to link a component to other pages
 import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+//import a hook to use the history function
+import { useHistory } from 'react-router-dom'
 
 //after we import BrowserRouter we need to wrap the routers
 //we have to add exact in order to retrieve the exact page otherwise will retrieve the first coincidence even though the rest of the path does not match
 //add a navigation bar to navigate around the application
 
-const NavBar = () => (
-  <div className="nav bg-light d-flex justify-content-between">
-    <Link className="nav-link" to="/">
-      Home
-    </Link>
-    <Link className="nav-link" to="/login">
-      Login
-    </Link>
-    <Link className="nav-link" to="/register">
-      Register
-    </Link>
-    <Link className="nav-link" to="/services">
-      Services
-    </Link>
-    <Link className="nav-link" to="/maintain">
-      Maintain
-    </Link>
-  </div>
-)
+const NavBar = () => {
+  const dispatch = useDispatch()
+  const { auth } = useSelector((state) => ({ ...state }))
+  const history = useHistory()
+
+  const logout = () => {
+    dispatch({
+      type: 'LOGOUT',
+      payload: null,
+    })
+    window.localStorage.removeItem('auth')
+    //push the user to the login once it is logged out
+    history.push('/login')
+  }
+
+  return (
+    <div className="nav bg-light d-flex justify-content-between">
+      <Link className="nav-link" to="/">
+        Home
+      </Link>
+
+      <Link className="nav-link" to="/services">
+        Services
+      </Link>
+      <Link className="nav-link" to="/maintain">
+        Maintain
+      </Link>
+
+      {auth !== null && (
+        //add the pointer class to add a pointer to the logout function
+        <href className="nav-link pointer" onClick={logout}>
+          Logout
+        </href>
+      )}
+
+      {auth === null && (
+        <>
+          {''}
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+          <Link className="nav-link" to="/register">
+            Register
+          </Link>
+          {''}
+        </>
+      )}
+    </div>
+  )
+}
 
 export default NavBar
