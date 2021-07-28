2b4dc928197b587ec03091189df1b82f36fab200
Veronica2484 <veronicacondori24@gmail.com>
2021-07-28 08:12:10 +0100

    settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 //import functionalities from react -router-dom
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-//App component will import the rest of the components
-//of the entire application based on its path as ./booking/Home
-import Home from './booking/Home'
-import Login from './auth/Login'
-import Register from './auth/Register'
-import Services from './booking/Services'
-import Maintain from './adm/Maintain'
-import NavBar from './components/NavBar'
 //import those to handle notification
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+//import
+import NavBar from './components/NavBar'
+import PrivateRoute from './components/PrivateRoute'
+
+//App component will import the rest of the components
+//of the entire application based on its path as ./booking/Home
+//Components
+import Home from './booking/Home'
+import Login from './auth/Login'
+import Register from './auth/Register'
+import Services from './user/Services'
+import ServicesOwner from './user/ServicesOwner'
+import NewItem from './items/newItems'
 
 // we need to exacute the Nav function into the App
 function App() {
@@ -30,8 +35,9 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-        <Route exact path="/services" component={Services} />
-        <Route exact path="/maintain" component={Maintain} />
+        <PrivateRoute exact path="/services" component={Services} />
+        <PrivateRoute exact path="/services/owner" component={ServicesOwner} />
+        <PrivateRoute exact path="/items/new" component={NewItem} />
       </Switch>
     </BrowserRouter>
   )
