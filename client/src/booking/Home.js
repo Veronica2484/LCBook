49f62db803ffc9354373f38e05c410fab3925bba
Veronica2484 <veronicacondori24@gmail.com>
2021-07-29 21:47:47 +0100

    Add list of items

--- client/src/booking/Home.js
+++ client/src/booking/Home.js
@@ -1,7 +1,35 @@
+import { useState, useEffect } from 'react'
+import { listOfItems } from '../actions/item'
+import SmallCard from '../components/cards/SmallCard'
 //create a arrow function
 //useSelector takes a function and returns the state
 const Home = () => {
-  return <div className="container-fluid h1 p-5 text-center">Home</div>
+  //create the item state
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    loadAllitems()
+  }, [])
+
+  const loadAllitems = async () => {
+    let res = await listOfItems()
+    setItems(res.data)
+  }
+
+  return (
+    <>
+      <div className="container-fluid banner p-5 text-center">
+        <h1> List of items </h1>
+      </div>
+      <div className="container-fluid">
+        <br />
+        {/* <pre>{JSON.stringify(hotels, null, 4)}</pre> */}
+        {items.map((h) => (
+          <SmallCard key={h._id} h={h} />
+        ))}
+      </div>
+    </>
+  )
 }
 
 //exporting this we can import and use it in another component
