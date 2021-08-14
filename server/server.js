9ab5a7f063322747c6b32f20cfdc99438c98125d
Veronica2484 <veronicacondori24@gmail.com>
2021-08-14 13:14:26 +0100

    server

--- server/server.js
+++ server/server.js
@@ -70,9 +70,9 @@ readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)))
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')))
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'client', 'build', 'index.html')
+    res.sendFile(index)
   })
 } else {
   app.get('/', (req, res) => {
