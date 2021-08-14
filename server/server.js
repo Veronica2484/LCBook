db7fa8d9ec0c6898e166229f7e8b43e281f77bb6
Veronica2484 <veronicacondori24@gmail.com>
2021-08-14 18:12:56 +0100

    server

--- server/server.js
+++ server/server.js
@@ -72,7 +72,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(resolvedDirectory, '../client/build')))
   app.get('*', (req, res) =>
     // res.sendFile(path.resolve(__dirname, "../client", "public", "index.html"))
-    res.sendFile(path.resolve(resolvedDirectory, '/client/build/index.html'))
+    res.sendFile(path.resolve(resolvedDirectory, '../client/build/index.html'))
   )
 } else {
   app.get('/', (req, res) => {
