4d1396ee88e2b811ccdbc3352d5092cde57e2f69
Veronica2484 <veronicacondori24@gmail.com>
2021-08-14 16:47:58 +0100

    deployment

--- server/server.js
+++ server/server.js
@@ -69,7 +69,7 @@ readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)))
 //condition for deployment
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')))
+  const dir = app.use(express.static(path.join(__dirname, '../client/build')))
   app.get('*', (req, res) =>
     // res.sendFile(path.resolve(__dirname, "../client", "public", "index.html"))
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
